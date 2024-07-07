8958da021f06822a8023cae85224a54bc9ba5c8d
Aryan Singh <as10071999@gmail.com>
2024-07-08 01:14:48 +0530

    added tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,7 @@ import { useNavigate } from "react-router-dom";
 const App = (props: Props) => {
   const navigate = useNavigate();
 
-  return (
-    <div className="container">
-      <br />
-      Hello World
-      <br />
-      <button
-        onClick={() => {
-          navigate("/home");
-        }}
-      >
-        Click Here to GO to HOME page
-      </button>
-    </div>
-  );
+  return <div className=" bg-red-200 w-screen h-screen">Hello World</div>;
 };
 
 export default App;
