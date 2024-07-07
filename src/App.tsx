51ad00961d56f2e542fd9898b8032602fc49f7d1
Aryan Singh <as10071999@gmail.com>
2024-07-08 02:43:12 +0530

    added qr code

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 type Props = {};
-
-import { useNavigate } from "react-router-dom";
+import qrImage from "./assets/image-qr-code.png";
 
 const App = (props: Props) => {
-  const navigate = useNavigate();
-
-  return <div className=" bg-red-200 w-screen h-screen">Hello World</div>;
+  return (
+    <div className="bg-[#D5E1EF] w-screen h-screen flex justify-center items-center">
+      <div className="w-[320px] h-[497px] shadow-lg bg-white rounded-[20px] flex flex-col items-center ">
+        <img
+          className="rounded-[10px] h-[288px] mt-[16px]"
+          src={qrImage}
+          width={288}
+        />
+        <div className="px-[16px] mt-[16px]  text-center font-bold text-2xl text-[#1F314F] font-outfit">
+          Improve your front-end skills by building projects
+        </div>
+        <div className="mt-[16px] px-[24px]  text-center text-[#7D889E] font-outfit">
+          Scan the QR code to visit Frontend Mentor and take your coding skills
+          to the next level
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
+    fontFamily: {
+      outfit: ["Outfit", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
